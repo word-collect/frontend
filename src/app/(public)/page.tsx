955f42d433321c54bed11f1ea942c9a56c031759
rooster man <roostermanred@gmail.com>
2025-07-07 19:38:53 -0500

    fix tab bar

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -21,7 +21,7 @@ export default function Page() {
             Improve your vocab with AI
           </h1>
           <p className="mt-8 text-lg font-medium text-pretty text-gray-400 sm:text-xl/8">
-            wordcollect helps you extract vocabulary words and phrases from
+            WordCollect helps you extract vocabulary words and phrases from
             files and stores them automatically so you can commit them to
             memory.
           </p>
