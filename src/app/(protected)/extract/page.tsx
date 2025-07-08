5f19e049ddad5dd818c65b792a86bfde5fda7f45
rooster man <roostermanred@gmail.com>
2025-07-08 16:40:25 -0500

    better extract page

--- src/app/(protected)/extract/page.tsx
+++ src/app/(protected)/extract/page.tsx
@@ -3,7 +3,7 @@ import { Suspense } from 'react'
 
 export default function ExtractPage() {
   return (
-    <section className="flex-1 mx-auto max-w-4xl px-2 sm:px-6 lg:px-8 pt-4">
+    <section className="flex-1 mx-auto max-w-4xl px-2 sm:px-6 lg:px-8 pt-4 flex flex-col justify-center">
       <Suspense fallback={null}>
         <Uploader />
       </Suspense>
--- src/components/uploader.tsx
+++ src/components/uploader.tsx
@@ -53,10 +53,10 @@ export const Uploader = () => {
 
   return (
     <>
-      <div className="col-span-full">
+      {/* <div className="col-span-full">
         <div
           className="mt-2 flex justify-center rounded-lg border border-dashed
-                     border-white/25 px-6 py-10 mb-4"
+                     border-white/25 px-6 py-10"
           onDragOver={(e) => e.preventDefault()}
           onDrop={(e) => {
             e.preventDefault()
@@ -92,15 +92,8 @@ export const Uploader = () => {
           </div>
         </div>
       </div>
-      <div className="relative my-12">
-        <div aria-hidden="true" className="absolute inset-0 flex items-center">
-          <div className="w-full border-t border-gray-500" />
-        </div>
-        <div className="relative flex justify-center">
-          <span className="bg-black px-2 text-sm text-gray-500">Or</span>
-        </div>
-      </div>
-      <form onSubmit={handleSubmit} className="mt-2 flex gap-2">
+      <span className="separator py-10">Or</span>
+      <form onSubmit={handleSubmit} className="flex gap-2 mb-8">
         <div className="flex flex-1 rounded-md bg-white outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
           <input
             id="search"
@@ -122,14 +115,80 @@ export const Uploader = () => {
         >
           Submit
         </button>
-      </form>
+      </form> */}
       {/* {fileName && <p className="text-sm text-gray-500">{fileName}</p>} */}
       {/* ── Real-time viewer ────────────────────────────────────────── */}
-      {fileName && (
+      {fileName ? (
         <section className="mt-4 w-full max-w-4xl">
           {/* <h2 className="font-medium mb-2">Extraction result</h2> */}
           <ExtractionViewerPush fileName={fileName} />
         </section>
+      ) : (
+        <>
+          <div className="col-span-full">
+            <div
+              className="mt-2 flex justify-center rounded-lg border border-dashed
+                     border-white/25 px-6 py-10"
+              onDragOver={(e) => e.preventDefault()}
+              onDrop={(e) => {
+                e.preventDefault()
+                if (fileName) return
+                void handleFile(e.dataTransfer.files?.[0])
+              }}
+            >
+              <div className="text-center">
+                <DocumentTextIcon
+                  aria-hidden="true"
+                  className="mx-auto size-12 text-gray-500"
+                />
+                <div className="mt-4 flex text-sm/6 text-gray-400">
+                  <label
+                    htmlFor="file-upload"
+                    className="relative cursor-pointer rounded-md bg-gray-900 font-semibold text-white focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 focus-within:ring-offset-gray-900 focus-within:outline-hidden hover:text-indigo-500"
+                  >
+                    <span>Upload a file</span>
+                    <input
+                      id="file-upload"
+                      name="file-upload"
+                      type="file"
+                      className="sr-only"
+                      disabled={!!fileName}
+                      onChange={(e) => handleFile(e.target.files?.[0])}
+                    />
+                  </label>
+                  <p className="pl-1">or drag and drop</p>
+                </div>
+                <p className="text-xs/5 text-gray-400">
+                  HTML, PDF, MD, TXT up to 10MB
+                </p>
+              </div>
+            </div>
+          </div>
+          <span className="separator py-10">Or</span>
+          <form onSubmit={handleSubmit} className="flex gap-2 mb-8">
+            <div className="flex flex-1 rounded-md bg-white outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+              <input
+                id="search"
+                name="search"
+                type="text"
+                placeholder="Provide a URL"
+                className="block min-w-0 grow px-3 py-1.5 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+                autoCapitalize="none"
+                suppressHydrationWarning
+                value={url}
+                disabled={!!fileName}
+                onChange={(e) => setUrl(e.target.value)}
+              />
+            </div>
+            <button
+              type="submit"
+              disabled={!!fileName}
+              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Submit
+            </button>
+          </form>
+        </>
       )}
     </>
   )
