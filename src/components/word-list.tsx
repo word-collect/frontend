cea62e84823d5487080a987980b527fa9db67e66
rooster man <roostermanred@gmail.com>
2025-07-08 16:48:56 -0500

    indigo buttons

--- src/components/word-list.tsx
+++ src/components/word-list.tsx
@@ -80,13 +80,13 @@ export default function Example() {
           <div className="flex flex-none items-center gap-x-4">
             <div className="flex flex-none items-center gap-x-4">
               <span
-                className="cursor-pointer hidden rounded-md bg-gray-100 px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-xs ring-1 ring-gray-300 ring-inset hover:bg-gray-50 sm:block"
+                className="cursor-pointer hidden rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:block"
                 onClick={() => handleDefine(item.word)}
               >
                 Define
               </span>
               <span
-                className="cursor-pointer hidden rounded-md bg-gray-100 px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-xs ring-1 ring-gray-300 ring-inset hover:bg-gray-50 sm:block"
+                className="cursor-pointer hidden rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:block"
                 onClick={() => handleRelate(item.word)}
               >
                 Relate
